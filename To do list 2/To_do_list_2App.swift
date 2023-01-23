f2fbf0a4c0ab7bd2cde34da80dce96de28639995
Erin Chon <echon@bsd220.org>
2023-01-23 09:35:40 -0600

    Initial Commit

---
+++ To do list 2/To_do_list_2App.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_list_2App.swift
+//  To do list 2
+//
+//  Created by Erin Chon on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct To_do_list_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
