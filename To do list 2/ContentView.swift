9545dad40af009599a0eec92790801e138a30d7a
Erin Chon <echon@bsd220.org>
2023-01-23 10:09:38 -0600

    REDSEIGN

--- To do list 2/ContentView.swift
+++ To do list 2/ContentView.swift
@@ -16,31 +16,40 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(toDoItems) { item in
-                    Text(item.description)
+                ForEach(toDoItems)
+                { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
                 }
-                .onMove { indices, newOffset in
-                    toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    .onMove { indices, newOffset in
+                        toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    }
+                    .onDelete { indexSet in
+                        toDoItems.remove(atOffsets: indexSet)
+                    }
                 }
-                .onDelete { indexSet in
-                    toDoItems.remove(atOffsets: indexSet)
-                }
+                .navigationBarTitle("To Do List", displayMode: .inline)
+                .navigationBarItems(leading: EditButton ())
             }
-            .navigationBarTitle("To Do List", displayMode: .inline)
-            .navigationBarItems(leading: EditButton ())
         }
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
         }
-    }
+        
+        struct ToDoItem: Identifiable {
+            var id = UUID()
+            var priority = String ()
+            var description = String ()
+            var dueDate = Date ()
+        }
     
-    struct ToDoItem: Identifiable {
-        var id = UUID()
-        var priority = String ()
-        var description = String ()
-        var dueDate = Date ()
-    }
-}
