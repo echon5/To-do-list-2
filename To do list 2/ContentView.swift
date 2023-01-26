375d0bd037f5f7fcb252081f5649e029ff03dcc7
Erin Chon <echon@bsd220.org>
2023-01-26 09:44:40 -0600

    Enabled data persistence

--- To do list 2/ContentView.swift
+++ To do list 2/ContentView.swift
@@ -50,10 +50,10 @@ struct ContentView: View {
             }
         }
         
-        struct ToDoItem: Identifiable {
+        struct ToDoItem: Identifiable, Codable {
             var id = UUID()
             var priority = String ()
             var description = String ()
             var dueDate = Date ()
         }
-    
+
